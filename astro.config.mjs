0b467781551cd2f401d31f0b53ee0a62f3ba1a86
otmh2992 <otmh.here@gmail.com>
2026-05-22 16:11:54 +0100

    fix astro cloudflare build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: "static",
+  output: 'server',
+
+  adapter: cloudflare({
+    imageService: 'compile'
+  }),
+
+  integrations: [react()],
+
+  vite: {
+    ssr: {
+      external: ['crypto']
+    }
+  }
 });
