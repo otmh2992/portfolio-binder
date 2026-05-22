51c7f03778d09bdb88f7609dc5db5e218f82334b
otmh2992 <otmh.here@gmail.com>
2026-05-22 13:57:11 +0100

    fix astro config build failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-<h1>ROOT OK 2</h1>
-
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
+  site: "https://planzzz.com",
 });
