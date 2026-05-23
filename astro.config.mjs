2c69c47144d8baed70991befa3b96590cd0cab4b
otmh2992 <otmh.here@gmail.com>
2026-05-23 17:12:04 +0100

    fix cloudflare pages build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
-    platformProxy: true
-  })
+    platformProxy: false
+  }),
 });
