111193e883ce1b2bcbf93a4709f96379c537f547
otmh2992 <otmh.here@gmail.com>
2026-05-26 13:12:00 +0100

    fix: clean SSR build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import react from "@astrojs/react";
 export default defineConfig({
   output: "server",
 
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 
   integrations: [react()],
 });
